fafe29159f6ce62b7d7cc9658cd438a8443a22bd
Ankit Mathur <ankitmathur@berkeley.edu>
2017-09-28 10:47:36 -0700

    remove skeleton comments from meeting

--- src/main/scala/opt/OptimizationSpecification.scala
+++ src/main/scala/opt/OptimizationSpecification.scala
@@ -1,20 +1,3 @@
-//Class Structure
-
-
-/* Constructor (1. Set of allowed instructions, 2. Query Plan, 3: Set of allowed transformations to the query plan)
- *
- */
-
-// 1. Allowed Instructions
-// Abstract - Instruction(Relations, Parameters), NOOP
-// Instruction implements checkSchema():Boolean
-// Relations base data, as well as instruction
-// .equals (equivalent)
-
-//2. Instruction Instr([A, instructionof(B, C, D)], Parameters)
-
-//3. Transformations: Instruction -> Instruction
-
 /* Set of allowed instructions */
 object validInstructionTypes {
   val validTypes : List[String] = List("join")
