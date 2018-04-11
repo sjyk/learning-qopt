762cea533c0abb43c56546feeb69589955723823
Ankit Mathur <ankitmathur@berkeley.edu>
2018-04-10 18:28:08 -0700

    implement SparkJoin and SparkRelation primitives for usage from SparkSession.sqlBetterJoin

--- src/main/scala/opt/SparkSQLTranslator.scala
+++ src/main/scala/opt/SparkSQLTranslator.scala
@@ -1,16 +1,48 @@
 
 package opt
 
-import org.apache.spark.sql.catalyst.plans.logical
-import dml.Join
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.plans.{Inner, logical}
+import org.apache.spark.sql.catalyst.analysis
+import dml.{DataRelationStub, Join}
+import opt.RelationStub
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.catalog.Catalog
+import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
+import org.apache.spark.sql.catalyst.encoders.RowEncoder
+import org.apache.spark.sql.catalyst.plans.logical.{BinaryNode, LogicalPlan}
+import org.apache.spark.sql.internal._
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
+class SparkRelationStub(var tableName : String,
+                        var sparkSession : SparkSession,
+                        var correspondingLogicalPlan : LogicalPlan,
+                        var evaluatedTable : Option[Dataset[Row]] = None,
+                        var cost : Double = 0,
+                        var p : ArrayBuffer[String] = ArrayBuffer())
+  extends RelationStub(tableName, mutable.Set(), cost, p) {
+  var count : Option[Long] = None
+  def getCount : Long = {
+    if (count.isEmpty) {
+      if (evaluatedTable.isDefined) {
+        count = Some(evaluatedTable.get.count())
+      } else {
+        val qe = sparkSession.sessionState.executePlan(correspondingLogicalPlan)
+        qe.assertAnalyzed()
+        val table_rows = new Dataset[Row](sparkSession, correspondingLogicalPlan, RowEncoder(qe.analyzed.schema)).count()
+        count = Some(table_rows)
+      }
+    }
+    count.get
+  }
+}
+
 class SparkJoin(var relations : ArrayBuffer[Either[RelationStub, QueryInstruction]],
-           var parameters : ArrayBuffer[ConstraintStub], var input : logical.Join) extends QueryInstruction("join") {
+           var parameters : ArrayBuffer[ConstraintStub], var input : logical.LogicalPlan, var sparkSession : SparkSession) extends QueryInstruction("join") {
+
+  var result : Option[Dataset[Row]] = None
 
   override def checkSchema(): Boolean = {
     true
@@ -20,6 +52,7 @@ class SparkJoin(var relations : ArrayBuffer[Either[RelationStub, QueryInstructio
     if (!checkSchema()) {
       throw new Exception("Schema validation failed for this object.")
     }
+    var joinLogicalPlan = SparkSQLTranslator.queryInstructionToLogicalPlan(this)
     var i = 0
     /* Resolve all Query Instructions first. */
     for (i <- relations.indices) {
@@ -29,19 +62,23 @@ class SparkJoin(var relations : ArrayBuffer[Either[RelationStub, QueryInstructio
       }
       relations.update(i, Left(newRelation))
     }
-    /* We have 2 relation stubs - in practice, we would kick join logic to the RDD - here we compute set intersect.*/
+    val qe = sparkSession.sessionState.executePlan(joinLogicalPlan)
+    qe.assertAnalyzed()
+    if (result.isEmpty) {
+      var table = new Dataset[Row](sparkSession, joinLogicalPlan, RowEncoder(qe.analyzed.schema))
+      result = Some(table)
+    }
     val leftTable = relations(0).left.get
     val rightTable = relations(1).left.get
-    /* we actually want to execute the Spark Join here because there's no other way to get the relationsize of the already joined operators. */
-    /* can we use the "output" list from logical.Join.output? */
-    val intersection = leftTable.relationContent & rightTable.relationContent
     val joinName = rightTable.relationName + " * " + leftTable.relationName
-    val cost = perJoinCost(leftTable.initCost, leftTable.relationContent.size, rightTable.initCost, rightTable.relationContent.size)
+    val lTableRowSize = leftTable.asInstanceOf[SparkRelationStub].getCount
+    val rTableRowSize = rightTable.asInstanceOf[SparkRelationStub].getCount
+    val cost = perJoinCost(leftTable.initCost, lTableRowSize, rightTable.initCost, rTableRowSize)
     val provenance = leftTable.provenance ++ rightTable.provenance
-    new RelationStub(joinName, intersection, cost, provenance)
+    new SparkRelationStub(joinName, sparkSession, joinLogicalPlan, result, cost, provenance)
   }
 
-  def perJoinCost(lCost : Double, lSize : Int, rCost : Double, rSize : Int) : Double = {
+  def perJoinCost(lCost : Double, lSize : Long, rCost : Double, rSize : Long) : Double = {
     val cost = lCost + rCost
     val IOCost = lSize * rSize
     cost + IOCost
@@ -84,33 +121,118 @@ class SparkJoin(var relations : ArrayBuffer[Either[RelationStub, QueryInstructio
   }
 }
 //
-class SparkSQLTranslator {
-  def sparkJoinToQIJoin(input : logical.Join) : SparkJoin = {
-    if (input.resolved) {
-      if (input.right.isInstanceOf[logical.Join]) {
-        new SparkJoin(ArrayBuffer(Left(sparkRelationToRelationStub(input.left)), Right(sparkJoinToQIJoin(input.right.asInstanceOf[logical.Join]))), ArrayBuffer(), input)
+object SparkSQLTranslator {
+  var relationReference : mutable.Map[String, UnresolvedRelation] = mutable.Map[String, UnresolvedRelation]()
+  var parentLogicalPlan : Option[logical.LogicalPlan] = None
+  var translatedChildIndex : Option[Int] = None
+  var sparkSession : Option[SparkSession] = None
+  var catalog : Option[Catalog] = None
+  var sqlConfig : Option[SQLConf] = None
+
+  def logicalPlanToQI(input : logical.LogicalPlan, catalog : Catalog, sqlConf: SQLConf) : Either[RelationStub, QueryInstruction] = {
+    if (input.isInstanceOf[logical.Join]) {
+      val joinPlan = input.asInstanceOf[BinaryNode]
+      // either it's a [Join, Relation] or a [Relation, Relation]
+      if (joinPlan.left.isInstanceOf[logical.Join]) {
+        val rightPlan = logicalPlanToQI(joinPlan.right, catalog, sqlConf)
+        if (rightPlan.isRight) {
+          throw new Exception("Unexpected: Right plan of a join is a QueryInstruction")
+        }
+        val leftPlan = logicalPlanToQI(joinPlan.left, catalog, sqlConf)
+        if (leftPlan.isLeft) {
+          throw new Exception("Unexpected: Left plan of a join is a RelationStub")
+        }
+        Right(new SparkJoin(ArrayBuffer(Left(rightPlan.left.get), Right(leftPlan.right.get)), ArrayBuffer(), input, sparkSession.get))
       } else {
-        new SparkJoin(ArrayBuffer(Left(sparkRelationToRelationStub(input.left)), Left(sparkRelationToRelationStub(input.right))), ArrayBuffer(), input)
+        Right(new SparkJoin(
+          ArrayBuffer(
+            Left(logicalPlanToQI(joinPlan.left, catalog, sqlConf).left.get),
+            Left(logicalPlanToQI(joinPlan.right, catalog, sqlConf).left.get)),
+          ArrayBuffer(),
+          input,
+          sparkSession.get))
+      }
+    } else if (input.isInstanceOf[UnresolvedRelation]) {
+      Left(sparkRelationToRelationStub(input, sqlConf))
+    } else {
+      for (i <- input.children.indices) {
+        var c = input.children(i)
+        if (c.isInstanceOf[logical.Join]) {
+          parentLogicalPlan = Some(input)
+          translatedChildIndex = Some(i)
+          return logicalPlanToQI(c, catalog, sqlConf)
+        }
+      }
+      // if it's some other instruction, just give up and stub it as a relation with no data.
+      Left(new RelationStub(input.getClass.getName, mutable.Set()))
+    }
+  }
+
+  def sparkRelationToRelationStub(input : logical.LogicalPlan, sqlConf : SQLConf) : opt.RelationStub = {
+    if (input.isInstanceOf[analysis.UnresolvedRelation]) {
+      val relation = input.asInstanceOf[analysis.UnresolvedRelation]
+      // stubbing actual data with an empty Set.
+      val tableName = relation.tableName
+      relationReference = relationReference + (tableName -> relation)
+      new RelationStub(tableName, mutable.Set())
+    } else {
+      throw new Exception("Trying to convert a non-LocalRelation object to RelationStub.")
+    }
+  }
+
+  def relationStubToSparkRelation(input : RelationStub) : Option[UnresolvedRelation] = {
+    relationReference get input.relationName
+  }
+
+  def queryInstructionToLogicalPlan(input : QueryInstruction): logical.LogicalPlan = {
+    // probably have to store some prelude to the actual joins to execute this.
+    if (input.isInstanceOf[Join]) {
+      val join = input.asInstanceOf[SparkJoin]
+      if (join.relations(0).isLeft && join.relations(1).isLeft) {
+        val joinPlan = logical.Join(
+          relationStubToSparkRelation(join.relations(0).left.get).get,
+          relationStubToSparkRelation(join.relations(1).left.get).get,
+          Inner,
+          None)
+        if (parentLogicalPlan.isDefined) {
+          var newChildren = ArrayBuffer[LogicalPlan]()
+          for (i <- parentLogicalPlan.get.children.indices) {
+            if (i != translatedChildIndex.get) {
+              val c = parentLogicalPlan.get.children(i)
+              newChildren += c
+            } else {
+              newChildren += joinPlan
+            }
+          }
+          parentLogicalPlan.get.withNewChildren(newChildren.toSeq)
+          parentLogicalPlan.get
+        } else {
+          joinPlan
+        }
+      } else {
+        val joinPlan = logical.Join(
+          relationStubToSparkRelation(join.relations(0).left.get).get,
+          queryInstructionToLogicalPlan(join.relations(1).right.get),
+          Inner,
+          None)
+        if (parentLogicalPlan.isDefined) {
+          var newChildren = ArrayBuffer[LogicalPlan]()
+          for (i <- parentLogicalPlan.get.children.indices) {
+            if (i != translatedChildIndex.get) {
+              val c = parentLogicalPlan.get.children(i)
+              newChildren += c
+            } else {
+              newChildren += joinPlan
+            }
+          }
+          parentLogicalPlan.get.withNewChildren(newChildren.toSeq)
+          parentLogicalPlan.get
+        } else {
+          joinPlan
+        }
       }
     } else {
-      throw new Exception("Spark logical plan is not resolved")
+      throw new Exception("Currently don't know how to translate any other kind of QueryInstruction to a logicalPlan.")
     }
-  }
-
-  def sparkRelationToRelationStub(input : logical.LogicalPlan) : opt.RelationStub = {
-    if (input.resolved) {
-      if (input.isInstanceOf[logical.LocalRelation]) {
-        val relation = input.asInstanceOf[logical.LocalRelation]
-        // stubbing actual data with an empty Set.
-        val sqlConfig = SparkSession.getActiveSession.map(_.sessionState.conf).getOrElse(SQLConf.getFallbackConf)
-        return new RelationStub(relation.computeStats(sqlConfig).hashCode.toString, mutable.Set())
-      }
-    }
-    throw new Exception("Exception trying to convert sparkRelation to RelationStub")
   }
 }
-
-// "Join" extends LogicalPlan. LogicalPlan extends QueryPlan which has an Output.
-// I need a sparkJoin->RLJoin RLJoin.left is going to be a QueryPlan that masks a relation. Getting that relation involves calling output.
-// I need to figure out which QueryPlans are Relations and which are Joins. That makes a QI join - the pure relations become
-//
